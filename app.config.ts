3a25395a5d050848e473040893f80018a1a1972e
Yasin Akbulut <akbulutyasin07@gmail.com>
2026-06-19 00:32:14 +0300

    chore: bump version to 1.0.1 for App Store resubmission

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ const googleReversedScheme = iosGoogleClientId
 const config: ExpoConfig = {
   name: 'Outflow',
   slug: 'outflow',
-  version: '1.0.0',
+  version: '1.0.1',
   owner: 'yasinakbulut',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
